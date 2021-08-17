ac8a3fae540a5ef80d82a6432d04d07ac4049a33
Alby Baby <albybaby250219@gmail.com>
2021-08-17 09:58:09 -0400

    updated security

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 import './style.scss';
 let [first , second , third] = ['Quality Assurance','Security','Bad Example'];
-
-
-document.querySelector('#app').innerHTML = `
-<h1>An outline on quality assurance and security </h1>
+let main = `<h1>An outline on quality assurance and security </h1>
 <p>The security and QA is an important factor in keeping our websites safe from attacks and to keep our code easier to read and understand.</p>
 <p>It can be divided in to three parts</p> 
 <ol>
@@ -12,10 +9,13 @@ document.querySelector('#app').innerHTML = `
 <li>${third}.</li>
 `;
 
-document.querySelector('#header').innerHTML = `
-<ul>
+document.querySelector('#app').innerHTML = `${main}`;
+
+let header = `<ul>
 <li><a href="./views/quality_assurance.html"> Quality Assurance</a></li>
 <li><a href="./views/security.html">Security</a></li>
 <li><a href="./views/bad_example.html">Bad Example</a></li>
 </ul>
 `;
+document.querySelector('#header').innerHTML = `${header}`;
+
